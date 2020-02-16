fed740f1ae04aad416733de9f25997238122eace
nadvolod <nadvolod@gmail.com>
2020-02-16 17:18:04 -0500

    working configuration

--- src/test/java/atda/AcceptanceTestDrivenAutomation.java
+++ src/test/java/atda/AcceptanceTestDrivenAutomation.java
@@ -1,4 +1,19 @@
 package atda;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AcceptanceTestDrivenAutomation {
+    @Test
+    public void shouldOpen()
+    {
+        WebDriver driver = getDriver();
+    }
+
+    private WebDriver getDriver() {
+        //Telling the system where to find chromedriver. On Windows you also need to add .exe
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        return new ChromeDriver();
+    }
 }
