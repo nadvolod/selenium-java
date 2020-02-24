08f4410f320e479d8edd3dca615e209c8908ade5
nadvolod <nadvolod@gmail.com>
2020-02-24 15:03:00 -0500

    remove all implementation details

--- src/test/java/atda/AcceptanceTestDrivenAutomationTest.java
+++ src/test/java/atda/AcceptanceTestDrivenAutomationTest.java
@@ -1,42 +1,11 @@
 package atda;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.Assert.assertTrue;
 
 public class AcceptanceTestDrivenAutomationTest {
-    WebDriver driver;
-    @Before
-    public void setup()
-    {
-        driver = getDriver();
-    }
-    @After
-    public void cleanup()
-    {
-        driver.quit();
-    }
     @Test
     public void shouldOpen()
     {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.open();
-        assertTrue(loginPage.isLoaded());
-    }
-
-    @Test
-    public void shouldLogin()
-    {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.open();
-        assertTrue(loginPage.isLoaded());
-
-        loginPage.login("standard_user", "secret_sauce");
-        assertTrue(new ProductsPage(driver).isLoaded());
     }
 
     private WebDriver getDriver() {
