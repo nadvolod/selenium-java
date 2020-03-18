2134028de22ce9f94d9801056c282757820bddb8
nadvolod <nadvolod@gmail.com>
2020-03-18 18:39:49 -0400

    final solution

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class HelloSeleniumTest {
     @Test
