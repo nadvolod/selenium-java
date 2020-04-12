ab2f596b43dc6414c2aaac3032f180a49a3ee788
nadvolod <nadvolod@gmail.com>
2020-04-12 18:15:01 -0400

    add final exam

--- src/test/java/locating/elements/LocatingElementsTest.java
+++ src/test/java/locating/elements/LocatingElementsTest.java
@@ -1,6 +1,7 @@
 package locating.elements;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
 
@@ -75,43 +78,50 @@ public class LocatingElementsTest {
     @Test
     public void locatorExam()
     {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //2. navigate to the URL
         driver.get("https://www.saucedemo.com/");
 
-        //css ID
-
-        //Input element with class form_input
-
-        //Direct child
-
-        //Using attributes. Input with id='user-name'
-
+        //Use CSS Selectors
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector(".btn_action")).click();
 
+        //Use CSs/Xpath
         driver.findElement(By.xpath("//*[@class='btn_primary btn_inventory']")).click();
+        driver.findElement(By.cssSelector("#shopping_cart_container")).click();
+        driver.findElement(By.cssSelector(".btn_action.checkout_button")).click();
+
+        //Use the best locator
+        driver.findElement(By.id("first-name")).sendKeys("first name");
+        driver.findElement(By.id("last-name")).sendKeys("last name");
+        driver.findElement(By.id("postal-code")).sendKeys("zip");
+        driver.findElement(By.cssSelector(".btn_primary.cart_button")).click();
+
+        driver.findElement(By.linkText("FINISH")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("#checkout_complete_container")).isDisplayed());
 
 
-        driver.findElement(By.id("user-name"));
-        //Name
-        //driver.findElement(By.name("name of locator"));
-        //Class name
-        driver.findElement(By.className("form_input"));
-        //Tag name
-        driver.findElement(By.tagName("input"));
-        //Css selector
-        //#user-name
-        driver.findElement(By.cssSelector("#user-name"));
-        //Xpath
-        // //*[@id="user-name"]
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
-
-        //2. navigate to the URL
-        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
-        //Link text
-        driver.findElement(By.linkText("Click me using this link text!"));
-        //Partial link text
-        driver.findElement(By.partialLinkText("link text!"));
+//        driver.findElement(By.id("user-name"));
+//        //Name
+//        //driver.findElement(By.name("name of locator"));
+//        //Class name
+//        driver.findElement(By.className("form_input"));
+//        //Tag name
+//        driver.findElement(By.tagName("input"));
+//        //Css selector
+//        //#user-name
+//        driver.findElement(By.cssSelector("#user-name"));
+//        //Xpath
+//        // //*[@id="user-name"]
+//        driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+//
+//        //2. navigate to the URL
+//        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+//        //Link text
+//        driver.findElement(By.linkText("Click me using this link text!"));
+//        //Partial link text
+//        driver.findElement(By.partialLinkText("link text!"));
     }
 }
