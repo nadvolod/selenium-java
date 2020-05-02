c50c158e778bc831b52c8b4e434adfc732444617
nadvolod <nadvolod@gmail.com>
2020-05-02 16:23:30 -0400

    add windows chromedriver

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -7,10 +7,25 @@ public class HelloSeleniumTest {
     @Test
     public void firstTest()
     {
-        //Telling the system where to find chromedriver. On Windows you also need to add .exe
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        /*
+        * Don't forget that chromedriver and Chrome browser must always
+        * be in sync. It's very likely that one day you will open your
+        * code and things just aren't working. That's because
+        * Chrome automatically updated and your chromedriver
+        * doesn't support the new Chrome browser.
+        * */
+
+        //Telling the system where to find chromedriver on mac
+        //System.setProperty("webdriver.chrome.driver", "resources/mac/chromedriver");
+
+        //The path of chromedriver for windows
+        System.setProperty("webdriver.chrome.driver", "resources/windows/chromedriver.exe");
+
+        //Create a new chrome driver for us to use to automate Chrome browser
         WebDriver driver = new ChromeDriver();
+        //Go to a URL
         driver.get("https://www.saucedemo.com/");
+        //Kill the browser
         driver.quit();
     }
 }
