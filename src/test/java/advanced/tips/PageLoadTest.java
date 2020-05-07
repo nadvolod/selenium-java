ca68b4301cb4327a5ba65df4b04f9ca76554426a
nadvolod <nadvolod@gmail.com>
2020-05-07 15:41:24 -0400

    add page load tests comparison

--- src/test/java/advanced/tips/PageLoadTest.java
+++ src/test/java/advanced/tips/PageLoadTest.java
@@ -2,7 +2,9 @@ package advanced.tips;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,9 +12,24 @@ public class PageLoadTest {
     private WebDriver driver;
 
     @Test
-    public void checkPageLoad() {
+    public void unecessaryPageLoadCheck() {
         driver = getDriver();
         driver.navigate().to("https://www.saucedemo.com/");
+        //Our check to see that the page is loaded
+        Assert.assertTrue(driver.findElement(By.id("BAZINGA")).isDisplayed());
+    }
+
+    @Test
+    public void smarterTest() {
+        driver = getDriver();
+        driver.navigate().to("https://www.saucedemo.com/");
+
+        /**
+         * Instead of checking for a page load, we can just continue with
+         * our normal actions, the error will be exactly the same
+         * */
+        driver.findElement(By.id("BAZINGA")).sendKeys("standard_user");
+        //Continue the rest of the UI actions to login...
     }
 
     @After
@@ -28,7 +45,6 @@ public class PageLoadTest {
         }
         //Telling the system where to find chromedriver on mac
         System.setProperty("webdriver.chrome.driver", "resources/mac/chromedriver");
-
 
         return new ChromeDriver();
     }
