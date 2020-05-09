59f4dc476cce5d3dd894e542d708a98a37bb237c
nadvolod <nadvolod@gmail.com>
2020-05-09 14:19:19 -0400

    add 2 more tests

--- src/test/java/atda/AcceptanceTestDrivenAutomationTest.java
+++ src/test/java/atda/AcceptanceTestDrivenAutomationTest.java
@@ -4,26 +4,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class AcceptanceTestDrivenAutomationTest extends BaseTest {
 
     @Test
-    public void shouldOpen() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.open();
-
-        assertTrue("Login page should load successfully", loginPage.isLoaded());
-    }
-
-    @Test
     public void shouldLogin() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        assertTrue("Login page should load successfully", loginPage.isLoaded());
-
+        //I used to do this, now I learned it's not necessary
+        //assertTrue("Login page should load successfully", loginPage.isLoaded());
         loginPage.login("standard_user", "secret_sauce");
         assertTrue("Products page should open when we login", new ProductsPage(driver).isLoaded());
+    }
+
+    @Test
+    public void shouldAddOneItemToCart() {
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.open();
+        productsPage.addItemToCart();
+        assertEquals("1", productsPage.getCountOfItemsInCart());
+    }
+    @Test
+    public void shouldAddTwoItemsToCart() {
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.open();
+        productsPage.addItemToCart();
+        productsPage.addItemToCart();
+        assertEquals("2", productsPage.getCountOfItemsInCart());
     }
 }
--- src/test/java/atda/ProductsPage.java
+++ src/test/java/atda/ProductsPage.java
@@ -2,6 +2,8 @@ package atda;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductsPage extends BasePage{
 
@@ -11,5 +13,17 @@ public class ProductsPage extends BasePage{
 
     public boolean isLoaded() {
         return isElementDisplayed(By.id("inventory_container"));
+    }
+
+    public void open() {
+        driver.navigate().to("https://www.saucedemo.com/inventory.html");
+    }
+
+    public void addItemToCart() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("btn_primary"))).click();
+    }
+    public String getCountOfItemsInCart() {
+        return driver.findElement(By.className("shopping_cart_badge")).getText();
     }
 }
