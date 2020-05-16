3b049bc2abda1994d94fa86baa1deffc5b0ae17d
nadvolod <nadvolod@gmail.com>
2020-05-16 09:23:07 -0400

    delete unused import

--- src/test/java/HelloSelenium2Test.java
+++ src/test/java/HelloSelenium2Test.java
@@ -2,7 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 
