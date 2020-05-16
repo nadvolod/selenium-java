1285507c3f21fa042c111d6d824adc68e3d0d01e
nadvolod <nadvolod@gmail.com>
2020-05-16 07:38:51 -0400

    add windows code

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -6,8 +6,11 @@ public class HelloSeleniumTest {
     @Test
     public void firstTest()
     {
-        //Telling the system where to find chromedriver. On Windows you also need to add .exe
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        //Telling the system where to find chromedriver on Mac.
+        //System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        //Here's how to do it on Windows
+        System.setProperty("webdriver.chrome.driver", "resources/windows/chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.quit();
