b1c8d1818594f16c805233232107ceef4747137a
nadvolod <nadvolod@gmail.com>
2020-05-25 17:53:23 -0400

    all the annotations

--- src/test/java/HelloSelenium2Test.java
+++ src/test/java/HelloSelenium2Test.java
@@ -1,21 +1,46 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class HelloSelenium2Test {
+    private WebDriver driver;
+
     //This method will run once before all of the tests in our class
     @BeforeClass
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    @AfterClass
+    public static void afterClass() {
+        //We can perform any operation after every single test has been executed
+    }
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
     @Test
     public void smarterTest()
     {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
-        driver.quit();
+    }
+    @Test
+    public void smarterTest2()
+    {
+        driver.get("https://www.saucedemo.com/");
+    }
+    @Test
+    public void smarterTest3()
+    {
+        driver.get("https://www.saucedemo.com/");
     }
 }
