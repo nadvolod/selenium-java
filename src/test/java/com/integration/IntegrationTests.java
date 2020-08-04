f927ccce370af5cbcbe91593a7e1c8c46d55cf02
nadvolod <nadvolod@gmail.com>
2020-08-04 19:14:24 -0400

    add integration

---
+++ src/test/java/com/integration/IntegrationTests.java
@@ -0,0 +1,11 @@
+package com.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegrationTests {
+    @Test
+    public void integrationShouldPass() {
+        Assert.assertTrue(true);
+    }
+}
