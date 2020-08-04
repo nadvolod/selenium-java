ce7dc8839a27d7292e25769806558b87840bfc15
nadvolod <nadvolod@gmail.com>
2020-08-04 19:11:29 -0400

    add unit test

---
+++ src/test/java/com/unit/UnitTests.java
@@ -0,0 +1,11 @@
+package com.unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTests {
+    @Test
+    public void shouldPass() {
+        Assert.assertTrue(true);
+    }
+}
