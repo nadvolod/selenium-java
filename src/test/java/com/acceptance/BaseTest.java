663edd276f4b9ecf4ac96057265763310ae670db
nadvolod <nadvolod@gmail.com>
2020-08-11 16:51:57 -0400

    optimize imports

--- src/test/java/com/acceptance/BaseTest.java
+++ src/test/java/com/acceptance/BaseTest.java
@@ -4,21 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.TestName;
-import org.junit.runners.Parameterized;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BaseTest {
     public WebDriver driver;
