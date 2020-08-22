77b4151b7379bc7331272bcc24205bf4b527a185
nadvolod <nadvolod@gmail.com>
2020-08-22 12:46:52 -0400

    removed useless code

--- src/test/java/com/testproject/LoginFeatureTests.java
+++ src/test/java/com/testproject/LoginFeatureTests.java
@@ -1,16 +1,12 @@
 package com.testproject;
+
 import io.testproject.sdk.drivers.web.ChromeDriver;
-import io.testproject.sdk.internal.exceptions.AgentConnectException;
-import io.testproject.sdk.internal.exceptions.InvalidTokenException;
-import io.testproject.sdk.internal.exceptions.ObsoleteVersionException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.io.IOException;
 
 public class LoginFeatureTests {
 
@@ -19,22 +15,17 @@ public class LoginFeatureTests {
     @Before
     public void setUp() throws Exception {
         driver = new ChromeDriver(new ChromeOptions());
-        driver.report().step("Create new driver");
     }
 
     @After
-    public void tearDown() throws Exception {
-        driver.report().step("Quit driver");
+    public void tearDown() {
         driver.quit();
     }
     @Test
-    public void shouldOpenPage() throws InvalidTokenException, AgentConnectException, ObsoleteVersionException, IOException {
+    public void shouldOpenPage() {
         driver.get("https://example.cypress.io/commands/actions");
-        driver.report().step("Open url " + "https://example.cypress.io/commands/actions");
         driver.findElement(By.cssSelector(".action-email")).sendKeys("test@test.com");
-        driver.report().step("Find element " + ".action-email");
         Assert.assertTrue(true);
     }
-
 
 }
