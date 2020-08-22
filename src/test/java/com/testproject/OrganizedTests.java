e4afe4ed52164799ebe4a32471b67a9298144c97
nadvolod <nadvolod@gmail.com>
2020-08-22 13:53:03 -0400

    updating information

--- src/test/java/com/testproject/OrganizedTests.java
+++ src/test/java/com/testproject/OrganizedTests.java
@@ -1,25 +1,21 @@
 package com.testproject;
 
-import io.testproject.sdk.drivers.web.ChromeDriver;
-import io.testproject.sdk.internal.exceptions.AgentConnectException;
-import io.testproject.sdk.internal.exceptions.InvalidTokenException;
-import io.testproject.sdk.internal.exceptions.ObsoleteVersionException;
+import io.testproject.sdk.drivers.web.SafariDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
-import java.io.IOException;
+public class OrganizedTests {
 
-public class LoginFeatureTests {
-
-    private ChromeDriver driver;
+    private WebDriver driver;
 
     @Before
-    public void setUp() throws InvalidTokenException, AgentConnectException, ObsoleteVersionException, IOException {
-        driver = new ChromeDriver(new ChromeOptions());
+    public void setUp() throws Exception {
+        driver = new SafariDriver(new SafariOptions(), "com.testproject", "AcceptanceTests");
     }
 
     @After
