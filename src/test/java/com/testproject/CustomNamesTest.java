ab546118adecf8bb64246977bf74fabffc52a0fb
nadvolod <nadvolod@gmail.com>
2020-08-22 17:29:51 -0400

    custom names

---
+++ src/test/java/com/testproject/CustomNamesTest.java
@@ -0,0 +1,40 @@
+package com.testproject;
+
+import io.testproject.sdk.drivers.web.SafariDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+public class CustomNamesTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new SafariDriver(new SafariOptions(), "Sample Project", "New Job");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void shouldOpen() {
+        driver.get("https://example.cypress.io/commands/actions");
+        Assert.assertEquals("Cypress.io: Kitchen Sink", driver.getTitle());
+    }
+
+    @Test
+    public void shouldType() {
+        driver.get("https://example.cypress.io/commands/actions");
+        var inputField = driver.findElement(By.cssSelector(".action-email"));
+        inputField.sendKeys("test@test.com");
+        Assert.assertEquals("test@test.com",
+                inputField.getAttribute("value"));
+    }
+}
