3a00f7660b8924e3fa0f37aa93a276f60b2a7821
nadvolod <nadvolod@gmail.com>
2020-08-23 11:19:46 -0400

    add junit5

--- src/test/java/com/testproject/CustomNamesTest.java
+++ src/test/java/com/testproject/CustomNamesTest.java
@@ -1,24 +1,25 @@
 package com.testproject;
 
 import io.testproject.sdk.drivers.web.SafariDriver;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomNamesTest {
 
     private WebDriver driver;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
-        driver = new SafariDriver(new SafariOptions(), "Sample Project", "New Job");
+        driver = new SafariDriver(new SafariOptions(), "JUnit 5", "Junit 5 Job");
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         driver.quit();
     }
@@ -26,7 +27,7 @@ public class CustomNamesTest {
     @Test
     public void shouldOpen() {
         driver.get("https://example.cypress.io/commands/actions");
-        Assert.assertEquals("Cypress.io: Kitchen Sink", driver.getTitle());
+        assertEquals("Cypress.io: Kitchen Sink", driver.getTitle());
     }
 
     @Test
@@ -34,7 +35,7 @@ public class CustomNamesTest {
         driver.get("https://example.cypress.io/commands/actions");
         var inputField = driver.findElement(By.cssSelector(".action-email"));
         inputField.sendKeys("test@test.com");
-        Assert.assertEquals("test@test.com",
+        assertEquals("test@test.com",
                 inputField.getAttribute("value"));
     }
 }
