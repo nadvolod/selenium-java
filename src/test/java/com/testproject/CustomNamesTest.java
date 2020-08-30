35a31b702698ec7600c5ffd349985fc3535fa1f7
nadvolod <nadvolod@gmail.com>
2020-08-30 14:17:24 -0400

    tried to fix testng

--- src/test/java/com/testproject/CustomNamesTest.java
+++ src/test/java/com/testproject/CustomNamesTest.java
@@ -1,22 +1,21 @@
 package com.testproject;
 
 import io.testproject.sdk.drivers.web.ChromeDriver;
+import io.testproject.sdk.internal.exceptions.AgentConnectException;
+import io.testproject.sdk.internal.exceptions.InvalidTokenException;
+import io.testproject.sdk.internal.exceptions.ObsoleteVersionException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class CustomNamesTest {
 
     private WebDriver driver;
-
-    @BeforeTest
-    public void setUp() throws Exception {
-        driver = new ChromeDriver(new ChromeOptions());
-    }
 
     @AfterTest
     public void tearDown() {
@@ -24,13 +23,15 @@ public class CustomNamesTest {
     }
 
     @Test
-    public void shouldOpen() {
+    public void shouldOpen() throws InvalidTokenException, AgentConnectException, ObsoleteVersionException, IOException {
+        driver = new ChromeDriver(new ChromeOptions());
         driver.get("https://example.cypress.io/commands/actions");
         Assert.assertEquals("Cypress.io: Kitchen Sink", driver.getTitle());
     }
 
     @Test
-    public void shouldType() {
+    public void shouldType() throws InvalidTokenException, AgentConnectException, ObsoleteVersionException, IOException {
+        driver = new ChromeDriver(new ChromeOptions());
         driver.get("https://example.cypress.io/commands/actions");
         var inputField = driver.findElement(By.cssSelector(".action-email"));
         inputField.sendKeys("test@test.com");
