5d45bc50109e3ba5653b79bfdd99332852cfe367
nadvolod <nadvolod@gmail.com>
2020-09-22 13:49:19 -0400

    delete all unecessary resources

--- src/test/java/HelloSelenium2Test.java
+++ src/test/java/HelloSelenium2Test.java
@@ -1,20 +1,13 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 
 public class HelloSelenium2Test {
-    //This method will run once before all of the tests in our class
-    @BeforeClass
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
     @Test
-    public void smarterTest()
+    public void simpleTest()
     {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new SafariDriver();
         driver.get("https://www.saucedemo.com/");
         driver.quit();
     }
