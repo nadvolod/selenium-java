aa303ef70a13049fe4c2f825ff6657aeee0d7c6c
nadvolod <nadvolod@gmail.com>
2020-09-22 14:06:59 -0400

    add comments

--- src/test/java/HelloSelenium2Test.java
+++ src/test/java/HelloSelenium2Test.java
@@ -1,14 +1,28 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.FindsByName;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+/*
+* First shown by Simon Stewart 7/18/20
+* https://www.youtube.com/watch?time_continue=14&v=c163xQXiiEM&feature=emb_logo
+* */
 
 public class HelloSelenium2Test {
     @Test
     public void simpleTest()
     {
         WebDriver driver = new SafariDriver();
+        //Deprecated in 4.x
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
         driver.get("https://www.saucedemo.com/");
+        //Removed in 4.x
+        WebElement button = ((FindsByName)driver).findElementByName("LOGIN");
+        button.click();
+
         driver.quit();
     }
 }
