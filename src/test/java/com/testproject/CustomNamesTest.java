1f9c0a5a1edab83b97f89fc0e8086a32960a9572
nadvolod <nadvolod@gmail.com>
2020-09-26 13:52:13 -0400

    remove cypress

--- src/test/java/com/testproject/CustomNamesTest.java
+++ src/test/java/com/testproject/CustomNamesTest.java
@@ -3,8 +3,12 @@ package com.testproject;
 import io.testproject.sdk.drivers.web.SafariDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomNamesTest {
 
@@ -20,5 +24,21 @@ public class CustomNamesTest {
         driver.quit();
     }
 
+    @Test
+    public void shouldOpen() throws InterruptedException {
+        driver.navigate().to("https://ultimateqa.com/filling-out-forms/");
+        Thread.sleep(3000);
+        assertEquals("Filling Out Forms - Ultimate QA", driver.getTitle());
+    }
 
+    @Test
+    public void shouldType() throws InterruptedException {
+        driver.navigate().to("https://ultimateqa.com/filling-out-forms/");
+        driver.findElement(By.id("et_pb_contact_name_0")).sendKeys("Nikolay");
+        Thread.sleep(1000);
+        driver.findElement(By.name("et_builder_submit_button")).click();
+        Thread.sleep(1000);
+        var actualListItems = driver.findElements(By.xpath("//*[@class='et-pb-contact-message']/ul/li")).size();
+        assertEquals(1, actualListItems);
+    }
 }
