46121aecac19ccc007b4c55c041b15b0fa98d2ec
nadvolod <nadvolod@gmail.com>
2020-09-26 13:38:13 -0400

    update to remove cypress

--- src/test/java/com/testproject/SimpleTest.java
+++ src/test/java/com/testproject/SimpleTest.java
@@ -2,12 +2,13 @@ package com.testproject;
 
 import io.testproject.sdk.drivers.web.ChromeDriver;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleTest {
 
@@ -24,17 +25,20 @@ public class SimpleTest {
     }
 
     @Test
-    public void shouldOpen() {
-        driver.get("https://example.cypress.io/commands/actions");
-        Assert.assertEquals("Cypress.io: Kitchen Sink", driver.getTitle());
+    public void shouldOpen() throws InterruptedException {
+        driver.navigate().to("https://ultimateqa.com/filling-out-forms/");
+        Thread.sleep(3000);
+        assertEquals("Filling Out Forms - Ultimate QA", driver.getTitle());
     }
 
     @Test
-    public void shouldType() {
-        driver.get("https://example.cypress.io/commands/actions");
-        var inputField = driver.findElement(By.cssSelector(".action-email"));
-        inputField.sendKeys("test@test.com");
-        Assert.assertEquals("test@test.com",
-                inputField.getAttribute("value"));
+    public void shouldType() throws InterruptedException {
+        driver.navigate().to("https://ultimateqa.com/filling-out-forms/");
+        driver.findElement(By.id("et_pb_contact_name_0")).sendKeys("Nikolay");
+        Thread.sleep(1000);
+        driver.findElement(By.name("et_builder_submit_button")).click();
+        Thread.sleep(1000);
+        var actualListItems = driver.findElements(By.xpath("//*[@class='et-pb-contact-message']/ul/li")).size();
+        assertEquals(1, actualListItems);
     }
 }
