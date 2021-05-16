9f1412a29772db6c830e482b0944e688104be673
andreea-qa <andreea.draniceanu@gmail.com>
2021-05-16 14:40:12 +0300

    Added inputStream

--- src/test/java/yamlDataDriven/User.java
+++ src/test/java/yamlDataDriven/User.java
@@ -2,11 +2,14 @@ package yamlDataDriven;
 
 public class User {
 
-    private String username;
-    private String password;
+    public String username;
+    public String password;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public String getUsername() {
+        return username;
     }
+    public String getPassword() {
+        return password;
+    }
+
 }
